1212afa5eb0b3e96d3b586fa6b7db06f594d7bfb
yoshihiro ueda <ueda@do-luck.net>
2018-01-01 03:14:09 +0900

    commit

--- src/DownloadJPEG.java
+++ src/DownloadJPEG.java
@@ -9,41 +9,43 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DownloadJPEG {
-	public DownloadJPEG( String urlname, File DownloadFolder, String FileSeparator) throws Exception{
+	public DownloadJPEG(String urlname, File DownloadFolder, String FileSeparator) throws Exception {
 		URL url;
 		try {
-			//	ネットに接続
-			url = new URL( urlname);
+			// ネットに接続
+			url = new URL(urlname);
 			HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
 			urlcon.setRequestMethod("GET");
 			urlcon.setAllowUserInteraction(false);
 			urlcon.setInstanceFollowRedirects(false);
 			urlcon.connect();
-			//	status code check
+			// status code check
 			int httpStatusCode = urlcon.getResponseCode();
-			if ( HttpURLConnection.HTTP_OK != httpStatusCode){
+			if (HttpURLConnection.HTTP_OK != httpStatusCode) {
 				throw new Exception();
 			}
-			//	バイナリデータのダウンロード
-			DataInputStream dataInStream = new DataInputStream( urlcon.getInputStream());
-			String saveName = DownloadFolder.getPath()+FileSeparator;
+			// バイナリデータのダウンロード
+			DataInputStream dataInStream = new DataInputStream(urlcon.getInputStream());
+			String saveName = DownloadFolder.getPath() + FileSeparator;
 			String urlnames[] = urlname.split("/");
-			saveName = saveName+urlnames[ urlnames.length-1];
-//			System.out.println( "saveName="+saveName);
-			DataOutputStream dataOutStream = new DataOutputStream( new BufferedOutputStream (new FileOutputStream(saveName)));
-			byte [] b =new byte[4096];
-			int readByte =0;
-			while( -1 != ( readByte=dataInStream.read(b))){
-				dataOutStream.write( b, 0 , readByte);
+			saveName = saveName + urlnames[urlnames.length - 1];
+			// System.out.println( "saveName="+saveName);
+			DataOutputStream dataOutStream = new DataOutputStream(
+					new BufferedOutputStream(new FileOutputStream(saveName)));
+			byte[] b = new byte[4096];
+			int readByte = 0;
+			while (-1 != (readByte = dataInStream.read(b))) {
+				dataOutStream.write(b, 0, readByte);
 			}
-			//	後始末
+			// 後始末
+			dataOutStream.flush();
 			dataInStream.close();
 			dataOutStream.close();
-			
+
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		
+
 	}
 }
